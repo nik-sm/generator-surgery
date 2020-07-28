71a15576828b8269d8df74d2496ec61ff532d009
Jung Yeon Park <jpark0@gmail.com>
2020-07-28 09:13:33 -0400

    - Run mgan_vanilla_vae_cs

--- scripts/run_experiments.sh
+++ scripts/run_experiments.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-[[ $# -eq 2 ]] || { echo "Required args: model_name, img_dir" >&2; exit 1; }
+[[ $# -eq 3 ]] || { echo "Required args: model_name, img_dir, device" >&2; exit 1; }
 
 MODEL=$1
 
-CUDA_VISIBLE_DEVICES=0 python run_experiments.py --img_dir $2 \
+CUDA_VISIBLE_DEVICES=$3 python run_experiments.py --img_dir $2 \
 --model ${MODEL} \
